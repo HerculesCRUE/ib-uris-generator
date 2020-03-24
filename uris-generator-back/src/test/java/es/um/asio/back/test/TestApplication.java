bd659d06eb11109d7df70c088e6daaad681b2b48
Rubén Gavilán <rgavilan@izertis.com>
2020-03-24 16:48:53 +0100

    Tests unitarios

--- uris-generator-back/src/test/java/es/um/asio/back/test/TestApplication.java
+++ uris-generator-back/src/test/java/es/um/asio/back/test/TestApplication.java
@@ -3,7 +3,10 @@ package es.um.asio.back.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import es.um.asio.service.mapper.MapperConfig;
 
@@ -19,5 +22,15 @@ public class TestApplication {
      */
     public static void main(final String[] args) {
         SpringApplication.run(TestApplication.class, args);
+    }
+    
+    /**
+     * Creates the password encoder BCrypt.
+     *
+     * @return The password encoder.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
