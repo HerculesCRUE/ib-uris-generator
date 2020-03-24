a6fc9adb82fff7400367145d3587506321ee1c46
Rubén Gavilán <rgavilan@izertis.com>
2020-03-24 16:15:05 +0100

    Initial commit

---
+++ uris-generator-service/src/main/java/es/um/asio/service/ServiceConfig.java
@@ -0,0 +1,29 @@
+package es.um.asio.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import es.um.asio.service.repository.RepositoryConfig;
+
+/**
+ * Service Spring configuration.
+ */
+@Configuration
+@ComponentScan(excludeFilters = { @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "*..solr..*") })
+@Import({RepositoryConfig.class})
+public class ServiceConfig {
+    /**
+     * Creates the password encoder BCrypt.
+     *
+     * @return The password encoder.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
