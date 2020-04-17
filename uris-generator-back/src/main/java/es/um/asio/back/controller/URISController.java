71413ccf6d09c5d4cbddf8f3980f378cc483c158
Alejandro Martino Uria <amartino@izertis.com>
2020-04-17 13:47:34 +0200

    SEMANTMURC-113 new params language and university

--- uris-generator-back/src/main/java/es/um/asio/back/controller/URISController.java
+++ uris-generator-back/src/main/java/es/um/asio/back/controller/URISController.java
@@ -1,5 +1,7 @@
 package es.um.asio.back.controller;
 
+import java.util.HashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.um.asio.abstractions.constants.Constants;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -21,70 +24,112 @@ public class URISController {
 	/** The logger. */
 	private final Logger logger = LoggerFactory.getLogger(URISController.class);
 	
-    /**
-     * Retrieve URI.
-     *
-     * @param input the input
-     * @return the string uri
-     */
-    @PostMapping(URISController.Mappings.RESOURCE_ID)
+	/**
+    * Creates the resourceID URI
+    * <p>
+    * Example
+    * <p>
+    * <pre>
+    * {
+    *    obj: 
+    *    {
+	*      class=es.um.asio.service.util.data.ConceptoGrupo, 
+	*      entityId=null, 
+	*      version=0, 
+	*      idGrupoInvestigacion=E0A6-01, 
+	*      numero=5, 
+	*      codTipoConcepto=DESCRIPTORES, 
+	*      texto=LENGUAJES PLASTICOS
+	*    },
+	*    language: es,
+	*    university: https://www.um.es
+    * }
+    * </pre>
+    * @param input HashMap.
+    * @return URI for the input property.
+    */
+    @SuppressWarnings({ "rawtypes", "unused" })
+	@PostMapping(URISController.Mappings.RESOURCE_ID)
     public String createResourceID(@RequestBody final Object input) {
         logger.info(input.toString());
+        HashMap map = (HashMap) input;
+        Object obj = map.get(Constants.OBJECT);
+        String className = (String) map.get(Constants.CLASS_NAME);
+        String language = (String) map.get(Constants.LANGUAGE);
+    	String university = (String) map.get(Constants.UNIVERSITY);
         
-        // Example input: 
-        /*
-         * {
-         * @class=es.um.asio.service.util.dummy.data.ConceptoGrupoDummy, 
-         * entityId=null, 
-         * version=0, 
-         * idGrupoInvestigacion=E0A6-01, 
-         * numero=5, 
-         * codTipoConcepto=DESCRIPTORES, 
-         * texto=LENGUAJES PLASTICOS}
-         */
         // FIXME
         return "http://www.jan107.es/10886753";
     }
     
     /**
-     * Creates the property URI.
-     *
-     * @param input the input
-     * @return the string
-     */
+    * Creates the property URI
+    * <p>
+    * Example
+    * <p>
+    * <pre>
+    * {
+    *   obj: {
+    *      class=es.um.asio.service.util.data.ConceptoGrupo, 
+    *      entityId=null, 
+    *      version=0, 
+    *      idGrupoInvestigacion=E0A6-01, 
+    *      numero=5, 
+    *      codTipoConcepto=DESCRIPTORES, 
+    *      texto=LENGUAJES PLASTICOS
+    *    },
+    *    property: idGrupoInvestigacion,
+    *    resourceID: http://example.org/1321/012/1/2012%2F05%2F,
+    *    language: es,
+    *    university: https://www.um.es
+    *  }
+    * </pre>
+    * @param input HashMap.
+    * @return URI for the input property.
+    */
+    @SuppressWarnings({ "rawtypes", "unused" })
     @PostMapping(URISController.Mappings.PROPERTY_URI)
     public String createPropertyURI(@RequestBody final Object input) {
-        logger.info(input.toString());
+        logger.info("Creating property URI");
+
+        HashMap map = (HashMap) input;
+        Object obj = map.get(Constants.OBJECT);
+        String className = (String) map.get(Constants.CLASS_NAME);
+        String property = (String) map.get(Constants.PROPERTY);
+        String resourceID = (String) map.get(Constants.RESOURCE_ID);
+        String language = (String) map.get(Constants.LANGUAGE);
+    	String university = (String) map.get(Constants.UNIVERSITY);
         
-        // Example input: 
-        /*
-         * {
-         * @class=es.um.asio.service.util.dummy.data.ConceptoGrupoDummy, 
-         * entityId=null, 
-         * version=0, 
-         * idGrupoInvestigacion=E0A6-01, 
-         * numero=5, 
-         * codTipoConcepto=DESCRIPTORES, 
-         * texto=LENGUAJES PLASTICOS}
-         */
         // FIXME
         return "http://www.w3.org/2001/asio-rdf/3.0#";
     }
     
     /**
-     * Creates the resource type URI.
-     *
-     * @param input the input
-     * @return the string
+     * Creates the resource type URI
+     * <p>
+     * Example
+     * <p>
+     * <pre>
+     * {
+ 	 *   className: ConceptoGrupo,
+ 	 *   language: es, 
+ 	 *   university: https://www.um.es
+     * }
+     * </pre>
+     * @param input HashMap.
+     * @return URI for the input property.
      */
+    @SuppressWarnings({ "rawtypes", "unused" })
     @PostMapping(URISController.Mappings.RESOURCE_TYPE_URI)
-    public String createResourceTypeURI(@RequestBody final String className) {
-        logger.info("ClassName: " + className);
-        // Example input:
-        /*
-         * ConceptoGrupoDummy
-         */
-        // FIXME
+    public String createResourceTypeURI(@RequestBody final Object input) {
+    	logger.info("Creating resource type uri: ");
+    	
+    	HashMap map = (HashMap) input;
+    	String className = (String) map.get(Constants.CLASS_NAME);
+    	String language = (String) map.get(Constants.LANGUAGE);
+    	String university = (String) map.get(Constants.UNIVERSITY);
+    	
+    	// FIXME
         return "http://example.org/" + className;
     }
 	
